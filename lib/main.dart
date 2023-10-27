bea3a669b0d9f7cf8bfe6faf4ef29744f61c8d71
Hayato <hayato.314.nico@gmail.com>
2023-10-28 00:03:08 +0900

    123 テーマの変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kcolorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromRGBO(255, 96, 59, 181),
+);
+
 void main() {
   runApp(
     MaterialApp(
       // theme: ThemeData(useMaterial3: true),
       theme: ThemeData().copyWith(
           useMaterial3: true,
-          scaffoldBackgroundColor: Color.fromARGB(255, 220, 189, 252)),
+          colorScheme: kcolorScheme,
+          appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: kcolorScheme.onPrimaryContainer,
+            foregroundColor: kcolorScheme.primaryContainer,
+          )),
       home: const Expenses(),
     ),
   );
