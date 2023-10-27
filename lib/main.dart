8d85c803b8378d949b9e80f76107add7d7dd163d
Hayato <hayato.314.nico@gmail.com>
2023-10-28 00:28:34 +0900

    124 スタイルを色々変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
-var kcolorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromRGBO(255, 96, 59, 181),
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(74, 36, 219, 210),
 );
 
 void main() {
@@ -10,12 +10,28 @@ void main() {
     MaterialApp(
       // theme: ThemeData(useMaterial3: true),
       theme: ThemeData().copyWith(
-          useMaterial3: true,
-          colorScheme: kcolorScheme,
-          appBarTheme: AppBarTheme().copyWith(
-            backgroundColor: kcolorScheme.onPrimaryContainer,
-            foregroundColor: kcolorScheme.primaryContainer,
-          )),
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 14),
+            ),
+      ),
       home: const Expenses(),
     ),
   );
